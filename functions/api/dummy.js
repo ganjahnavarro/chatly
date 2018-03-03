b0494921a1bfd81ecc067c98abcdfbf759713826
Ganjah <ganjah@gnjb.fx553>
2018-03-03 18:05:27 +0800

    added dummy data

---
+++ functions/api/dummy.js
@@ -0,0 +1,105 @@
+export const categories = [
+    { id: 1, name: 'Pizza' },
+    { id: 2, name: 'Chicken & Pasta' },
+    { id: 3, name: 'Appetizers & Desserts' },
+    { id: 4, name: 'Beverages' }
+];
+
+export const items = [
+    {
+        id: 1,
+        categoryId: 1,
+        name: 'EXTRAVAGANZZA',
+        description: `Here's what Domino's is famous for!`,
+        imageURL: 'https://www.dominospizza.ph/uploads/files/products/pizza/Extravaganzza.jpg',
+        price: 209
+    },
+    {
+        id: 2,
+        categoryId: 1,
+        name: 'KALAMATA TOMATO',
+        description: 'Taste the delicious Greek Pizza!',
+        imageURL: 'https://www.dominospizza.ph/uploads/files/products/pizza/Kalamata.jpg',
+        price: 189
+    },
+    {
+        id: 3,
+        categoryId: 1,
+        name: 'CHICKEN BBQ SAUSAGE',
+        description: 'Delectably made with flavorful ingredients.',
+        imageURL: 'https://www.dominospizza.ph/uploads/files/products/pizza/Chicken-BBQ-Sausage-250px-PNG.jpg',
+        price: 209
+    },
+    {
+        id: 4,
+        categoryId: 2,
+        name: 'CREAMY ALFREDO SPAGHETTI',
+        description: 'Enjoy our own version of the creamy goodness of Pasta Alfredo made with rich sauce and topped with mushrooms and shredded cheddar cheese.',
+        imageURL: 'https://www.dominospizza.ph/uploads/files/products/sides%20desserts/Creamy-Alfredo-Spaghetti.png',
+        price: 139
+    },
+    {
+        id: 5,
+        categoryId: 2,
+        name: 'MEATY BOLOGNESE SPAGHETTI',
+        description: 'Relish the taste of our all-time favorite meaty spaghetti, for kids and adults alike.',
+        imageURL: 'https://www.dominospizza.ph/uploads/files/products/sides%20desserts/Meaty-Bolognese-Spaghetti.png',
+        price: 139
+    },
+    {
+        id: 6,
+        categoryId: 2,
+        name: 'CHEESY BAKED MACARONI',
+        description: 'Oven baked macaroni, blended with our special red sauce topped with melted mozarella and cheddar cheese.',
+        imageURL: 'https://www.dominospizza.ph/uploads/files/products/sides%20desserts/Cheesy-Baked-Macaroni-250px.png',
+        price: 139
+    },
+    {
+        id: 7,
+        categoryId: 3,
+        name: 'MARBLED COOKIE BROWNIE (2PCS)',
+        description: 'A warm and chewy blend of butterscotch cookie and fudge brownie - now available in 2 pcs.',
+        imageURL: 'https://www.dominospizza.ph/uploads/files/products/sides%20desserts/Marbled-Cookie-Brownie-2pcs-250px-v2.png',
+        price: 49
+    },
+    {
+        id: 8,
+        categoryId: 3,
+        name: 'SPECIALTY CHICKEN - ALOHA BARBECUE',
+        description: 'Breaded, bite-sized tender chicken breast drizzled with BBQ Sauce, then topped with Mozzarella Cheese, and Pineapple Chunks.',
+        imageURL: 'https://www.dominospizza.ph/uploads/files/products/sides%20desserts/Aloha-Barbecue-for-website-250px.png',
+        price: 129
+    },
+    {
+        id: 9,
+        categoryId: 3,
+        name: 'POTATO WEDGES WITH BBQ DIP',
+        description: 'Enjoy our golden, oven-baked potato wedges with our barbecue dip! Pair it with your pizza as a side dish or enjoy it as a snack.',
+        imageURL: 'https://www.dominospizza.ph/uploads/files/products/sides%20desserts/Potato-Wedges.png',
+        price: 99
+    },
+    {
+        id: 10,
+        categoryId: 4,
+        name: 'COKE 1.5L',
+        description: 'Coke 1.5L',
+        imageURL: 'https://www.dominospizza.ph/uploads/files/products/beverages/Coca-Cola-Bottle_15.jpg',
+        price:
+    },
+    {
+        id: 11,
+        categoryId: 4,
+        name: 'MINUTE MAID FRESH',
+        description: '250ml',
+        imageURL: 'https://www.dominospizza.ph/uploads/files/products/beverages/MMFresh.jpg',
+        price: 29
+    },
+    {
+        id: 12,
+        categoryId: 4,
+        name: 'DOMINO\'S BOTTLED WATER',
+        description: '500ml',
+        imageURL: 'https://www.dominospizza.ph/uploads/files/products/beverages/dominos-water.jpg',
+        price: 29
+    }
+];
